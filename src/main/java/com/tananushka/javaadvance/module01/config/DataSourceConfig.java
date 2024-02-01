7bd539364cabad1de7a8864b79ae3caa28139905
Kir Tananushka <kir.tananushka@verifone.com>
2024-02-01 19:34:19 +0400

    Add solution for Task 2 - Conditional Configuration

--- src/main/java/com/tananushka/javaadvance/module01/config/DataSourceConfig.java
+++ src/main/java/com/tananushka/javaadvance/module01/config/DataSourceConfig.java
@@ -2,7 +2,7 @@ package com.tananushka.javaadvance.module01.config;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,11 +11,15 @@ import javax.sql.DataSource;
 
 @Configuration
 @AllArgsConstructor
+@Slf4j
 public class DataSourceConfig {
+    
     private final DataSourceProperties dataSourceProperties;
 
     @Bean
+    @ConditionalOnProperty(name = "custom.datasource.enable", havingValue = "true", matchIfMissing = true)
     public DataSource dataSource() {
+        log.debug("\n\n>>>>> Creating custom data source <<<<<\n");
         return DataSourceBuilder.create()
                 .url(dataSourceProperties.getUrl())
                 .username(dataSourceProperties.getUsername())
---
+++ src/test/java/com/tananushka/javaadvance/module01/config/DataSourceFallbackTest.java
@@ -0,0 +1,24 @@
+package com.tananushka.javaadvance.module01.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.TestPropertySource;
+
+import javax.sql.DataSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@TestPropertySource(properties = "custom.datasource.enable=false")
+class DataSourceFallbackTest {
+
+    @Autowired
+    private ApplicationContext context;
+
+    @Test
+    void shouldUseDefaultDataSource_whenCustomDataSourceIsDisabled() {
+        assertThat(context.getBean(DataSource.class)).isNotNull();
+    }
+}
