b75e07369738dc13d2a96398c600939dc22a01cd
Kir Tananushka <kir.tananushka@verifone.com>
2024-02-01 20:13:08 +0400

    Add solution for Task 3 - Spring Profiles

--- src/test/java/com/tananushka/javaadvance/module01/repository/BookRepositoryTest.java
+++ src/test/java/com/tananushka/javaadvance/module01/repository/BookRepositoryTest.java
@@ -3,11 +3,16 @@ package com.tananushka.javaadvance.module01.repository;
 import com.tananushka.javaadvance.module01.model.Book;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace.NONE;
 
 @DataJpaTest
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = NONE)
 class BookRepositoryTest {
 
     @Autowired
